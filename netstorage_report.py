72265438b7cc4387be3b14490486d35f888e6c2a
Juan Luis Baptiste <juan.baptiste@gmail.com>
2016-04-06 01:15:29 -0500

    Update version.

--- netstorage_report.py
+++ netstorage_report.py
@@ -14,7 +14,7 @@ import smtplib
 from email.mime.text import MIMEText
 from socket import error,gaierror
 
-__version__ = "0.1.0-11-g34de706"
+__version__ = "0.1.0-14-g55e57dc"
 # Configure the logging level and stream to stdout to see the logs.
 logging.basicConfig(level=logging.ERROR,
                     format="%(levelname)s[%(name)s.%(funcName)s:%(lineno)s] %(message)s",
