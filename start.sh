554cc0577c774c4135952595b8226415f88ca757
Juan Luis Baptiste <juan@juanbaptiste.tech>
2021-04-19 00:00:06 -0500

    Set CRONJOB_TIME on start

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 
 echo "SMTP_TO=$SMTP_TO" >> /root/vars.env
 echo "REMOTEPATH=$REMOTEPATH" >> /root/vars.env
+echo "${CRONJOB_TIME} root /run.sh > /var/log/cron.log 2>&1" > /etc/cron.d/netstorage
 
 crond
 while true
